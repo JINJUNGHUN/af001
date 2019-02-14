ae27c829783a3c0d898bfc353395415d11967960
JayJin <unisquarebsm@gmail.com>
2019-02-14 16:16:08 +0900

    asdlfjasdkfj

---
+++ db/migrate/20190214064723_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.integer :temperature
+      t.integer :time
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
